c0feedf3c631ba0d58ba10b9d92b9804e50f2bfb
丁国峰 <dingguofeng@zplay.cn>
2016-03-08 18:59:09 +0800

    添加单元测试

--- test/utils/encrypt.js
+++ test/utils/encrypt.js
@@ -4,15 +4,13 @@ var Encrypt = require('../../lib/utils/encrypt');
 describe("encrypt test!", function () {
     before('init bearcat', function (done) {
         Bearcat.createApp();
-        Bearcat.module(Encrypt,Encrypt.func);
-        Bearcat.use([Encrypt.id]);
         Bearcat.start(function(){
+            Bearcat.getBean(Encrypt);
             done();
         });
     });
     it("md5 test", function (done) {
         var test = "Hello world!";
-        console.log(Bearcat.getBean('encrypt'));
         var md5text = Bearcat.getBean('encrypt').md5(test, "hex");
         console.log(test, "(md5)", md5text,"\n");
         done();
